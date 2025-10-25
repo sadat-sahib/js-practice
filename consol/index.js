6ae8c972cd3a9aaf5a29e83185bfc49dc6cad533
musa <ahmadeyan22@gmail.com>
2025-10-25 21:53:08 +0430

    implement map

--- consol/index.js
+++ consol/index.js
@@ -3,9 +3,24 @@ const b = BigInt(9007199254740991);
 const c = 9007199254740992;
 const d = BigInt(9007199254740992);
 
-console.log(a === b);
-console.log(Number(a) === c);
-console.log(c === Number.MAX_SAFE_INTEGER + 1);
-console.log(d > a);
-console.log(typeof (a + 1n));
-console.log(Number(d) === c);
+// console.log(a === b);
+// console.log(Number(a) === c);
+// console.log(c === Number.MAX_SAFE_INTEGER + 1);
+// console.log(d > a);
+// console.log(typeof (a + 1n));
+// console.log(Number(d) === c);
+
+
+// ----------------------##########--------------------
+const Maybe = {
+  of: (value) => ({ value, map: f => value == null ? Maybe.nothing() : Maybe.of(f(value)) }),
+  nothing: () => ({ value: null, map: () => Maybe.nothing() })
+};
+
+const result = Maybe.of(5)
+  .map(x => x * 2)
+  .map(x => x > 15 ? null : x)
+  .map(x => x + 1)
+  .map(x => x.toString());
+
+console.log(result.value);
